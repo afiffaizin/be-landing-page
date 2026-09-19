e6dc5d663ff8420a9988b1c0ff27e12a32324f64
adeariansyah <projectalpha774@gmail.com>
2026-09-19 23:32:44 +0700

    fix: perbaiki error seeder bagian about section karena beda kolom

--- database/seeders/AboutSectionSeeder.php
+++ database/seeders/AboutSectionSeeder.php
@@ -24,19 +24,19 @@ class AboutSectionSeeder extends Seeder
                 'title' => 'Pemberdayaan Masyarakat',
                 'description' => 'Melaksanakan program pemberdayaan masyarakat melalui pelatihan, pendampingan, dan transfer teknologi tepat guna untuk meningkatkan kemandirian dan kesejahteraan.',
                 'image' => null,
-                'order' => 1,
+                'steps' => 1,
             ],
             [
                 'title' => 'Penerapan IPTEK',
                 'description' => 'Menerapkan hasil penelitian dan ilmu pengetahuan untuk menyelesaikan permasalahan nyata di masyarakat secara efektif dan berkelanjutan.',
                 'image' => null,
-                'order' => 2,
+                'steps' => 2,
             ],
             [
                 'title' => 'Kemitraan Berkelanjutan',
                 'description' => 'Membangun kemitraan jangka panjang dengan masyarakat, pemerintah daerah, dan pihak terkait untuk memastikan keberlanjutan program pengabdian.',
                 'image' => null,
-                'order' => 3,
+                'steps' => 3,
             ],
         ];
 
